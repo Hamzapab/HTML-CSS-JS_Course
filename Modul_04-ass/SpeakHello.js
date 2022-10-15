8082924c22bfc999a2b4eb9284666102714e2e04
HamzaWebDev <91342208+Hamzapab@users.noreply.github.com>
2022-10-15 21:45:25 +0200

    Delete Modul_04-ass directory

--- Modul_04-ass/SpeakHello.js
+++
@@ -1,9 +0,0 @@
-
-
-(function(){
-  var speakWord = "Hello";
-  var helloSpeaker = function speak(name) {
-    console.log(speakWord + " " + name);
-  };
-  window.helloSpeaker = helloSpeaker;
-})();
